8445292932df7f08f673fe9163c3038e31c2bb31
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-06-22 18:57:55 -0500

    cm11

--- level_tres_jeremy/cielo.js
+++
@@ -1,5 +0,0 @@
-let comenzar = document.getElementById("start");
-start.addEventListener("click", function(event){
-    document.getElementById("pantalla-incial").style.display = "none";
-    document.getElementById("pantalla-juego").style.display = "block";
-})
