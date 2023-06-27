af736d9f9ae4f317ce65c7a8de41c31437de2278
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-06-26 20:08:04 -0500

    cm20

--- Inferno/lvl_One_js.js
+++ Inferno/lvl_One_js.js
@@ -29,7 +29,7 @@ function carta(x,y,w,h,imagenFrente,imagenAtras){/*Funcion u objeto "carta". Es 
                 <img src="`+this.imagenFrente+`" alt="AvatarF" style="width:`+this.w+`px;height:`+this.h+`px;">
             </div>
             <div class="flip-cartaTemporal-back">
-                <img src="`+this.imagenAtras+`" alt="AvatarB" style="width:`+this.w+`px;height:`+this.h+`px;">
+                <img src="`+this.imagenAtras+`" alt="AvatarB" style="width: 100%; height: 100px;">
             </div>
         </div>
     </div> 
@@ -64,14 +64,14 @@ function cargaCartas(){/*Funcion de cartas*/
         if(i < 4){ y = 10; }/*Condicionales para modificar la posición en 'y' y posicionar las filas una debajo de otra*/
         if(i < 8 && i >3){ y = 130; }
         if(i < 12 && i >7){ y = 250; }
-        let cartaTemporal = new carta(modificador ,y ,100 ,100 ,'imagenes/back.png','imagenes/'+cartasTemporal[i]+'.png'); /*instanciamos el objeto carta pasándole los parámetros de x, y, w, h; también en primer lugar la imagen de espaldas y luego la imagen de frente individual que toma de la carpeta imágenes como ruta y luego del array de cartasTemporal la posición de i en ese momento */
+        let cartaTemporal = new carta(modificador ,y ,100 ,100 ,'img/fondo.png','img/'+cartasTemporal[i]+'.png'); /*instanciamos el objeto carta pasándole los parámetros de x, y, w, h; también en primer lugar la imagen de espaldas y luego la imagen de frente individual que toma de la carpeta imágenes como ruta y luego del array de cartasTemporal la posición de i en ese momento */
         lugar.insertAdjacentHTML("beforeend", cartaTemporal.template);/*Se inserta el código que se tiene como template de la carta y que antes del cierre del div de pantalla, se va a usar el objeto de carta temporal y accedemos al valor de template y lo va a ahcer por cada una de las iteraciones de 0 a 11 */
         modificador = modificador + 120;/*El modificador se le suma 120 para que la siguiente carta se sume en 120 la siguiente carta -> 10 130 */
     }
 }
 
 function pasarSiguienteNivel() {
-    // Aquí puedes redirigir al siguiente nivel "Heaven"
+    //Se redirige automáticamente al siguiente nivel una vez completado el primero
     window.location.href = "/heaven/cielo.html";
 }
 
--- heaven/cielo.js
+++ heaven/cielo.js
@@ -1,5 +1,20 @@
 const TOTAL_PREGUNTAS = 10;
-
+//base de datos de las preguntas
+const bd_juego = [
+    {
+        id:'A',
+    pregunta: '¿Cuál es el motivo principal por el cual las almas están rodeadas de una luz tenue y se mueven en un círculo? ',
+respuesta: "compromiso" },
+]
+//variables para controlar el tiempo 
+const timer = document.getElementById("tiempo");
+//tiempo del juego en segundos 
+const TIEMPO_DEL_JUEGO = 5;
+//variable que indica el tiempo restante 
+let timeleft = TIEMPO_DEL_JUEGO;
+//variable de maneja el contador 
+var countdown;
+//creamos letras de la 
 const container = document.querySelector(".container");
 for(let i = 1; i  <= TOTAL_PREGUNTAS; i++){
     const circle = document.createElement("div");
@@ -14,12 +29,28 @@ for(let i = 1; i  <= TOTAL_PREGUNTAS; i++){
     circle.style.left = `${x}px`;
     circle.style.top = `${y}px`;
 }
-
-
+//boton cemenzar 
 var comenzar = document.getElementById("start");
 comenzar.addEventListener("click",function(event){
     document.getElementById("pantalla-incial").style.display = "none";
     document.getElementById("pantalla-juego").style.display = "block";
+
+    largarTiempo()
+    cargarPregunta() 
 })
+function largarTiempo(){
+    countdown = setInterval(()=>{
+        //restar un segundo al tiempo restante 
+        timeleft--;
+        //acutalizamos el texto del cronometro 
+        timer.innerText = timeleft;
+        //si el tiempo llega  a 0, detener el cronometro 
+        if(timeleft<0){
+            clearInterval(countdown);
+            //mostrarPantallaFinal;
+            alert("se acabo el tiempo")
+        }
+    },1000);
+}
 
 
